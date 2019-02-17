ac90fef8f46b11e1ba61f650cd02887c7347a864
Chris James <chris@webglowit.net>
2019-02-18 03:22:27 +1100

    List animations and button

--- client/src/views/list.vue
+++ client/src/views/list.vue
@@ -2,12 +2,13 @@
   <div class="h-screen w-screen flex flex-col">
     <!-- SideNav -->
     <div
-      class="shadow-md flex flex-col justify-start absolute text-white h-screen bg-primary-light w-64"
+      class="sideNav shadow-md flex flex-col justify-start absolute text-white h-screen bg-primary-light w-64"
+      :class="{ showNav: showNav }"
     >
       <!-- Nav -->
       <div class="flex justify-between items-center shadow bg-primary-dark p-4">
         <span>Your name</span>
-        <button class="text-white">
+        <button class="text-white" @click="setNav(false)">
           <svgicon name="x" width="25" height="25"></svgicon>
         </button>
       </div>
@@ -19,9 +20,9 @@
       </div>
 
       <!-- Other users -->
-      <div v-else class="flex-1">
+      <div v-else class="flex-1 flex flex-col items-stretch">
         <!-- Users -->
-        <div v-for="user in otherUsers" class="p-4 border-b flex items-center">
+        <button v-for="user in otherUsers" class="p-4 border-b text-white flex items-center">
           <!-- Logo -->
           <div
             class="mr-4 flex-none flex items-center justify-center bg-primary text-white text-center chatimage"
@@ -30,13 +31,13 @@
           </div>
           <!-- Name -->
           <span class="font-bold truncate">Some Other user</span>
-        </div>
+        </button>
       </div>
     </div>
 
     <!-- Nav -->
     <div class="bg-primary p-4 text-white flex items-center shadow">
-      <button class="text-white mr-2">
+      <button class="text-white mr-2" @click="setNav(true)">
         <svgicon name="menu" width="25" height="25"></svgicon>
       </button>
       <span>Pushchat</span>
@@ -51,7 +52,7 @@
     <!-- v-else: Items List -->
     <div v-else class="flex-1 flex flex-col justify-start items-stretch">
       <!-- Chat Item -->
-      <div v-for="item in activeChats" class="p-4 border-b flex">
+      <button v-for="item in activeChats" class="p-4 border-b flex items-center">
         <!-- Logo -->
         <div
           class="mr-4 flex-none flex items-center justify-center bg-primary text-white text-center chatimage"
@@ -59,13 +60,13 @@
           <span class="uppercase text-xl">A</span>
         </div>
         <!-- Name and text -->
-        <div class="flex flex-col w-64">
-          <span class="font-bold">Alexader Smith</span>
+        <div class="flex flex-col text-left w-64">
+          <span class="font-bold mb-1">Alexader Smith</span>
           <span
             class="text-grey-dark truncate"
           >Here is some text which is going to be quite long and could me much longer</span>
         </div>
-      </div>
+      </button>
     </div>
   </div>
 </template>
@@ -77,6 +78,12 @@ import { Vue, Component, Prop } from "vue-property-decorator";
 export default class ListPage extends Vue {
   activeChats = new Array(3);
   otherUsers = new Array(4);
+
+  showNav = true;
+
+  setNav(show = false) {
+    this.showNav = show
+  }
 
   get noChats() {
     return this.activeChats.length === 0;
@@ -93,5 +100,13 @@ export default class ListPage extends Vue {
   border-radius: 50%;
   width: 50px;
   height: 50px;
+}
+
+.sideNav {
+  transition: 200ms transform cubic-bezier(0.19, 1, 0.22, 1);
+  transform: translateX(-100%);
+}
+.showNav {
+  transform: translateX(0);
 }
 </style>
